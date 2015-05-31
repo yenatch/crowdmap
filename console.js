b4caf98b34b35f5ba0b7c2a8f1886952fd98b2ba
yenatch <yenatch@gmail.com>
2015-05-31 14:59:44 -0700

    crop: map can't be bigger than 1300 blocks
    this probably ought to be in config

--- console.js
+++ console.js
@@ -27,10 +27,17 @@ function crop(x1, y1, x2, y2, etc) {
 
 	var last_w = map.width
 	var last_h = map.height
-	var width  = map.width  = x2 - x1
-	var height = map.height = y2 - y1
+	var width  = x2 - x1
+	var height = y2 - y1
 
 	if (last_w === width && last_h === height) return
+	if ((width + 6) * (height + 6) > 1300) {
+		if (!etc.override) {
+			console.log("map can't be more than 1300 blocks!")
+			return
+		}
+	}
+
 
 	var filler = etc.filler
 	if (typeof filler === 'undefined') {
@@ -44,18 +51,15 @@ function crop(x1, y1, x2, y2, etc) {
 	for (var y = y1; y < y2; y++)
 	for (var x = x1; x < x2; x++) {
 		if ((x >= 0 && x < last_w) && (y >= 0 && y < last_h)) {
-			blk.push(map.blockdata[last_w * y + x])
+			blk.push(map.getBlock(x, y))
 		} else {
 			blk.push(filler)
 		}
 	}
 
 	map.blockdata = blk
-
-	if (map === view.current_map) {
-		view.commit()
-		view.redraw = true
-	}
+	map.width = width
+	map.height = height
 }
 
 function resize(width, height, filler, map) {
